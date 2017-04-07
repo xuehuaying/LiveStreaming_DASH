75f27308b3b7885bfdc9cbf5c8e28112bb0fc508
melionel <lmamsat@hotmail.com>
2017-04-07 23:40:26 +0800

    add download metrics (bufferlevel and bitrate) in dash reference player

--- samples/dash-if-reference-player/app/main.js
+++ samples/dash-if-reference-player/app/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('DashPlayer', ['DashSourcesService', 'DashContributorsService', 'angular-flot']);
+var app = angular.module('DashPlayer', ['DashSourcesService', 'DashContributorsService', 'angular-flot', 'ngFileSaver']);
 
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
@@ -19,7 +19,7 @@ angular.module('DashContributorsService', ['ngResource']).factory('contributors'
     });
 });
 
-app.controller('DashController', function($scope, sources, contributors) {
+app.controller('DashController', function($scope, sources, contributors, FileSaver, Blob) {
 
 
     $scope.selectedItem = {url:"http://dash.edgesuite.net/akamai/bbb_30fps/bbb_30fps.mpd"};
@@ -220,224 +220,6 @@ app.controller('DashController', function($scope, sources, contributors) {
             $scope.doLoad();
         }
     }, $scope);
-
-    ////////////////////////////////////////
-    //
-    // DRM Events  //TODO Implement what is in eme-main and eme-index into this $scope.player to unify.  Add dialog in tab section for DRM license info.  Reinstate the DRM Options panel
-    //
-    ////////////////////////////////////////
-
-    // Listen for protection system creation/destruction by the $scope.player itself.  This will
-    // only happen in the case where we do not not provide a ProtectionController
-    // to the $scope.player via dashjs.MediaPlayer.attachSource()
-
-    //$scope.player.on(dashjs.MediaPlayer.events.PROTECTION_CREATED, function (e) {
-    //    var data = addDRMData(e.manifest, e.controller);
-    //    data.isPlaying = true;
-    //    for (var i = 0; i < $scope.drmData.length; i++) {
-    //        if ($scope.drmData[i] !== data) {
-    //            $scope.drmData[i].isPlaying = false;
-    //        }
-    //    }
-    //    $scope.safeApply();
-    //}, $scope);
-    //
-    //$scope.player.on(dashjs.MediaPlayer.events.PROTECTION_DESTROYED, function (e) {
-    //    for (var i = 0; i < $scope.drmData.length; i++) {
-    //        if ($scope.drmData[i].manifest.url === e.data) {
-    //            $scope.drmData.splice(i, 1);
-    //            break;
-    //        }
-    //    }
-    //    $scope.safeApply();
-    //}, $scope);
-
-
-    //var addDRMData = function(manifest, protCtrl) {
-    //
-    //    // Assign the session type to be used for this controller
-    //    protCtrl.setSessionType($("#session-type").find(".active").children().attr("id"));
-    //
-    //    var data = {
-    //        manifest: manifest,
-    //        protCtrl: protCtrl,
-    //        licenseReceived: false,
-    //        sessions: []
-    //    };
-    //    var findSession = function(sessionID) {
-    //        for (var i = 0; i < data.sessions.length; i++) {
-    //            if (data.sessions[i].sessionID === sessionID)
-    //                return data.sessions[i];
-    //        }
-    //        return null;
-    //    };
-    //    $scope.drmData.push(data);
-    //    $scope.safeApply();
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.KEY_SYSTEM_SELECTED, function(e) {
-    //        if (!e.error) {
-    //            data.ksconfig = e.data.ksConfiguration;
-    //        } else {
-    //            data.error = e.error;
-    //        }
-    //        $scope.safeApply();
-    //    }, $scope);
-    //
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.KEY_SESSION_CREATED, function(e) {
-    //        if (!e.error) {
-    //            var persistedSession = findSession(e.data.getSessionID());
-    //            if (persistedSession) {
-    //                persistedSession.isLoaded = true;
-    //                persistedSession.sessionToken = e.data;
-    //            } else {
-    //                var sessionToken = e.data;
-    //                data.sessions.push({
-    //                    sessionToken: sessionToken,
-    //                    sessionID: e.data.getSessionID(),
-    //                    isLoaded: true
-    //                });
-    //            }
-    //        } else {
-    //            data.error = e.error;
-    //        }
-    //        $scope.safeApply();
-    //    }, $scope);
-    //
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.KEY_SESSION_REMOVED, function(e) {
-    //        if (!e.error) {
-    //            var session = findSession(e.data);
-    //            if (session) {
-    //                session.isLoaded = false;
-    //                session.sessionToken = null;
-    //            }
-    //        } else {
-    //            data.error = e.error;
-    //        }
-    //        $scope.safeApply();
-    //    }, $scope);
-    //
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.KEY_SESSION_CLOSED, function(e) {
-    //        if (!e.error) {
-    //            for (var i = 0; i < data.sessions.length; i++) {
-    //                if (data.sessions[i].sessionID === e.data) {
-    //                    data.sessions.splice(i, 1);
-    //                    break;
-    //                }
-    //            }
-    //        } else {
-    //            data.error = e.error;
-    //        }
-    //        $scope.safeApply();
-    //    }, $scope);
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.KEY_STATUSES_CHANGED, function(e) {
-    //        var session = findSession(e.data.getSessionID());
-    //        if (session) {
-    //            var toGUID = function(uakey) {
-    //                var keyIdx = 0, retVal = "", i, zeroPad = function(str) {
-    //                    return (str.length === 1) ? "0" + str : str;
-    //                };
-    //                for (i = 0; i < 4; i++, keyIdx++)
-    //                    retVal += zeroPad(uakey[keyIdx].toString(16));
-    //                retVal += "-";
-    //                for (i = 0; i < 2; i++, keyIdx++)
-    //                    retVal += zeroPad(uakey[keyIdx].toString(16));
-    //                retVal += "-";
-    //                for (i = 0; i < 2; i++, keyIdx++)
-    //                    retVal += zeroPad(uakey[keyIdx].toString(16));
-    //                retVal += "-";
-    //                for (i = 0; i < 2; i++, keyIdx++)
-    //                    retVal += zeroPad(uakey[keyIdx].toString(16));
-    //                retVal += "-";
-    //                for (i = 0; i < 6; i++, keyIdx++)
-    //                    retVal += zeroPad(uakey[keyIdx].toString(16));
-    //                return retVal;
-    //            };
-    //            session.keystatus = [];
-    //            e.data.getKeyStatuses().forEach(function(status, key){
-    //                session.keystatus.push({
-    //                    key: toGUID(new Uint8Array(key)),
-    //                    status: status
-    //                });
-    //            });
-    //            $scope.safeApply();
-    //        }
-    //    }, $scope);
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.KEY_MESSAGE, function(e) {
-    //        var session = findSession(e.data.sessionToken.getSessionID());
-    //        if (session) {
-    //            session.lastMessage = "Last Message: " + e.data.message.byteLength + " bytes";
-    //            if (e.data.messageType) {
-    //                session.lastMessage += " (" + e.data.messageType + "). ";
-    //            } else {
-    //                session.lastMessage += ". ";
-    //            }
-    //            session.lastMessage += "Waiting for response from license server...";
-    //            $scope.safeApply();
-    //        }
-    //    }, $scope);
-    //
-    //    $scope.player.on(dashjs.MediaPlayer.events.LICENSE_REQUEST_COMPLETE, function(e) {
-    //        if (!e.error) {
-    //            var session = findSession(e.data.sessionToken.getSessionID());
-    //            if (session) {
-    //                session.lastMessage = "Successful response received from license server for message type '" + e.data.messageType + "'!";
-    //                data.licenseReceived = true;
-    //            }
-    //        } else {
-    //            data.error = "License request failed for message type '" + e.data.messageType + "'! " + e.error;
-    //        }
-    //        $scope.safeApply();
-    //    }, $scope);
-    //
-    //    return data;
-    //};
-    //
-    //$scope.delete = function(data) {
-    //    for (var i = 0; i < $scope.drmData.length; i++) {
-    //        if ($scope.drmData[i] === data) {
-    //            $scope.drmData.splice(i,1);
-    //            data.protCtrl.reset();
-    //            $scope.safeApply();
-    //        }
-    //    }
-    //};
-
-    //$scope.play = function (data) {
-    //    $scope.player.attachSource(data.manifest, data.protCtrl);
-    //    for (var i = 0; i < $scope.drmData.length; i++) {
-    //        var drmData = $scope.drmData[i];
-    //        drmData.isPlaying = !!(drmData === data);
-    //    }
-    //};
-
-    //$scope.doLicenseFetch = function () {
-    //    $scope.player.retrieveManifest($scope.selectedItem.url, function (manifest) {
-    //        if (manifest) {
-    //            var found = false;
-    //            for (var i = 0; i < $scope.drmData.length; i++) {
-    //                if (manifest.url === $scope.drmData[i].manifest.url) {
-    //                    found = true;
-    //                    break;
-    //                }
-    //            }
-    //            if (!found) {
-    //                var protCtrl = $scope.player.getProtectionController();
-    //                if ($scope.selectedItem.hasOwnProperty("protData")) {
-    //                    protCtrl.setProtectionData($scope.selectedItem.protData);
-    //                }
-    //                addDRMData(manifest, protCtrl);
-    //                protCtrl.initialize(manifest);
-    //            }
-    //        } else {
-    //            // Log error here
-    //        }
-    //    });
-    //};
 
     ////////////////////////////////////////
     //
@@ -686,6 +468,36 @@ app.controller('DashController', function($scope, sources, contributors) {
                 }
             }
         }
+    }
+
+    $scope.DownloadMetrics_BufferLevel = function (type) {
+        var metrics = $scope.player.getMetricsFor(type);
+        var text = '';
+
+        for (var i = 0; i < metrics['BufferLevel'].length; i ++)
+        {
+            text += metrics['BufferLevel'][i]['t'].getTime() + '\t' + metrics['BufferLevel'][i]['level'] + '\r\n';
+        }
+        var data = new Blob([text], { type: 'text/plain;charset=utf-8' });
+        FileSaver.saveAs(data, 'BufferLevel.txt');
+    }
+
+    $scope.DownloadMetrics_BitRate = function (type) {
+        var periodIdx = $scope.streamInfo.index;
+        var metrics = $scope.player.getMetricsFor(type);
+        var dashMetrics = $scope.player.getDashMetrics();
+
+        var text = '';
+
+        for (var i = 0; i < metrics['RepSwitchList'].length; i ++)
+        {
+            var repSwitch = metrics['RepSwitchList'][i];
+            var bitrate = Math.round(dashMetrics.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000);
+
+            text += metrics['RepSwitchList'][i]['t'].getTime() + '\t' + bitrate + '\r\n';
+        }
+        var data = new Blob([text], { type: 'text/plain;charset=utf-8' });
+        FileSaver.saveAs(data, 'Bitrate.txt');
     }
 
      $scope.initChartingByMediaType = function(type) {
