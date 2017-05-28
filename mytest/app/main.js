c9a74d70f3137ae44865ae1ebb05124b60da8ba4
xuehuaying <xuehuaying175@sina.com>
2017-05-28 17:49:02 +0800

    fix download metrics format

--- mytest/app/main.js
+++ mytest/app/main.js
@@ -21,9 +21,14 @@ app.controller('DashController', function($scope, FileSaver, Blob) {
         var metrics = $scope.player.getMetricsFor(type);
         var text = '';
 
+        var startTime;
+
         for (var i = 0; i < metrics['BufferLevel'].length; i ++)
         {
-            text += metrics['BufferLevel'][i]['t'].getTime() + '\t' + metrics['BufferLevel'][i]['level'] + '\r\n';
+            if (i == 0)
+                startTime = metrics['BufferLevel'][i]['t'].getTime();
+            var curTime = metrics['BufferLevel'][i]['t'].getTime() - startTime;
+            text += curTime + ',' + metrics['BufferLevel'][i]['level'] + '\r\n';
         }
         var data = new Blob([text], { type: 'text/plain;charset=utf-8' });
         FileSaver.saveAs(data, 'BufferLevel.txt');
@@ -36,12 +41,22 @@ app.controller('DashController', function($scope, FileSaver, Blob) {
 
         var text = '';
 
-        for (var i = 0; i < metrics['RepSwitchList'].length; i ++)
+        // for (var i = 0; i < metrics['RepSwitchList'].length; i ++)
+        // {
+        //     var repSwitch = metrics['RepSwitchList'][i];
+        //     var bitrate = Math.round(dashMetrics.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000);
+        //
+        //     text += metrics['RepSwitchList'][i]['t'].getTime() + '\t' + bitrate + '\r\n';
+        // }
+        for (var i = 0; i < metrics['SchedulingInfo'].length; i++)
         {
-            var repSwitch = metrics['RepSwitchList'][i];
-            var bitrate = Math.round(dashMetrics.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000);
-
-            text += metrics['RepSwitchList'][i]['t'].getTime() + '\t' + bitrate + '\r\n';
+            var scheduleInfo = metrics['SchedulingInfo'][i];
+            if (scheduleInfo.mediaType == type && scheduleInfo.state == 'executed' && scheduleInfo.type == 'MediaSegment')
+            {
+                var qualityId = scheduleInfo.quality + 1;
+                var bitRate = Math.round(dashMetrics.getBandwidthForRepresentation(qualityId.toString(), periodIdx) / 1000);
+                text += scheduleInfo.startTime + ',' + bitRate + '\r\n';
+            }
         }
         var data = new Blob([text], { type: 'text/plain;charset=utf-8' });
         FileSaver.saveAs(data, 'Bitrate.txt');
